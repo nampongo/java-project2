9af05af7f9778cdce9e4d84530a1bd46ad314cc3
sdw504512 <sdw504512@gmail.com>
2023-04-19 14:23:08 +0900

    feat: javaInputTest 추가

--- src/main/java/com/example/javaproject2/week1/JavaInputTest.java
+++ src/main/java/com/example/javaproject2/week1/JavaInputTest.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 public class JavaInputTest {
     /*
         param1 : ~~
-        param2 : ~~
+        param2 : ~~~
                 */
     public static void main(String[] args) throws IOException {
         JavaInput javaInput = new JavaInput();
