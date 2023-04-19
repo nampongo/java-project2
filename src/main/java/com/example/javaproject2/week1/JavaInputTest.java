90e5770d5722fde5d6bb1fd19083f13b666ceaea
sdw504512 <sdw504512@gmail.com>
2023-04-19 14:20:32 +0900

    week1 javaInputTest 추가

---
+++ src/main/java/com/example/javaproject2/week1/JavaInputTest.java
@@ -0,0 +1,14 @@
+package com.example.javaproject2.week1;
+
+import java.io.IOException;
+
+public class JavaInputTest {
+    /*
+        param1 : ~~
+        param2 : ~~
+                */
+    public static void main(String[] args) throws IOException {
+        JavaInput javaInput = new JavaInput();
+        javaInput.readTwoChars();
+    }
+}
