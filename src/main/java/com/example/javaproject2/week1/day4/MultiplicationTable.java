734418773f9b86bd82c87b236e99ba67fd43a46a
sdw504512 <sdw504512@gmail.com>
2023-04-20 13:14:44 +0900

    feat: MultiplicationTable 2단

--- src/main/java/com/example/javaproject2/week1/day4/MultiplicationTable.java
+++ src/main/java/com/example/javaproject2/week1/day4/MultiplicationTable.java
@@ -2,9 +2,7 @@ package com.example.javaproject2.week1.day4;
 
 public class MultiplicationTable {
     public static void main(String[] args) {
-        // 2 * 1 = 2
-        // 2 * 2 = 4
-        System.out.println("2 " + "* " + "1 " + "= " + "2");
-        System.out.printf("%d * %d = %d", 2, 2, 4);
+        for(int i = 1; i < 10; i++)
+            System.out.printf("%d * %d = %d\\n", 2, i, 2 * i);
     }
 }
---
+++ src/main/java/com/example/javaproject2/week1/day4/PrintfDate.java
@@ -0,0 +1,7 @@
+package com.example.javaproject2.week1.day4;
+
+public class PrintfDate {
+    public static void main(String[] args) {
+        System.out.printf("%d-%02d-%02d", 2024, 1, 9);
+    }
+}
