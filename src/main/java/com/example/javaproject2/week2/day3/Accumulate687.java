55295f8b322f748bade3d3d6363c146c1e6b5bfd
sdw504512 <sdw504512@gmail.com>
2023-04-27 16:23:15 +0900

    feat: week2 day4

--- src/main/java/com/example/javaproject2/week2/day3/Accumulate687.java
+++ src/main/java/com/example/javaproject2/week2/day3/Accumulate687.java
@@ -3,17 +3,17 @@ package com.example.javaproject2.week2.day3;
 public class Accumulate687 {
     public static void main(String[] args) {
         int num = 687;
-        int answer = 0;
+        int answer = 0;         // 누적할 변수 선언
 
-        answer += num % 10;
+        answer += num % 10;     // 나머지 구해 누적하기
         System.out.printf("answer: %d\n", answer);
-
         num /= 10;
-        answer += num % 10;
+
+        answer += num % 10;     // 나머지 구해 누적하기
         System.out.printf("answer: %d\n", answer);
-
         num /= 10;
-        answer += num % 10;
+
+        answer += num % 10;     // 나머지 구해 누적하기
         System.out.printf("answer: %d\n", answer);
     }
 }
