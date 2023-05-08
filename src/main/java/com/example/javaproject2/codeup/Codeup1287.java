1005827e7ffb65554364e940579551e4aebd5ade
sdw504512 <sdw504512@gmail.com>
2023-05-08 09:26:45 +0900

    feat: week4 day1

---
+++ src/main/java/com/example/javaproject2/codeup/Codeup1287.java
@@ -0,0 +1,19 @@
+package com.example.javaproject2.codeup;
+
+import java.util.Scanner;
+
+public class Codeup1287 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        String blc = "", res = "";
+
+        for (int i = 0; i < num; i++)
+            blc += "*";
+
+        for (int i = 1; i <= 9; i++) {
+            res += blc;
+            System.out.println(res);
+        }
+    }
+}
